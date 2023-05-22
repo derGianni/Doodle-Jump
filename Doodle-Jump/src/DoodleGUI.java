bc892d590a81fe0bea93c84a105ce3f7d52638f9
LorenzWilhelm <128386054+LorenzWilhelm@users.noreply.github.com>
2023-05-22 15:20:21 +0200

    Add files via upload

--- Doodle-Jump/src/DoodleGUI.java
+++ Doodle-Jump/src/DoodleGUI.java
@@ -6,7 +6,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.concurrent.TimeUnit;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -17,7 +19,7 @@ import javax.swing.JLayeredPane;
 import javax.swing.JOptionPane;
 import javax.swing.border.EmptyBorder;
 
-public class DoodleGUI extends JFrame implements ActionListener,  KeyListener {
+public class DoodleGUI extends JFrame implements ActionListener, KeyListener {
 
 	private JPanel contentPane;
 	private JPanel contentPane2;
@@ -26,41 +28,37 @@ public class DoodleGUI extends JFrame implements ActionListener,  KeyListener {
 	DoodleSteuerung dieDoodleSteuerung;
 	DoodleGUI dieDoodleGUI;
 	private Timer tim;
+	
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		/*EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					DoodleGUI frame = new DoodleGUI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});*/
+		/*
+		 * EventQueue.invokeLater(new Runnable() { public void run() { try { DoodleGUI
+		 * frame = new DoodleGUI(); frame.setVisible(true); } catch (Exception e) {
+		 * e.printStackTrace(); } } });
+		 */
 		DoodleGUI dieDoodleGUI = new DoodleGUI();
 		dieDoodleGUI.setVisible(true);
-	
+
+
 	}
-	
+
 	public void verloren() {
 		UIManager.put("OptionPane.yesButtonText", "Neustart");
-        UIManager.put("OptionPane.noButtonText", "Schliessen");
-      int wahl =  JOptionPane.showConfirmDialog(contentPane, "Verloren", "verloren", JOptionPane.YES_NO_OPTION);
-      if(wahl == JOptionPane.YES_OPTION) {
-    	  
-    	  tim.stop();
-    	  dispose();
-    	  dieDoodleGUI = new DoodleGUI();
-    	  dieDoodleGUI.setVisible(true);
+		UIManager.put("OptionPane.noButtonText", "Schliessen");
+		int wahl = JOptionPane.showConfirmDialog(contentPane, "Verloren", "verloren", JOptionPane.YES_NO_OPTION);
+		if (wahl == JOptionPane.YES_OPTION) {
 
-      }
-      else {
-    	System.exit(0);  
-      }
+			tim.stop();
+			dispose();
+			dieDoodleGUI = new DoodleGUI();
+			dieDoodleGUI.setVisible(true);
+
+		} else {
+			System.exit(0);
+		}
 	}
 
 	/**
@@ -70,69 +68,50 @@ public class DoodleGUI extends JFrame implements ActionListener,  KeyListener {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 500, 800);
-		//Das panel hat die Größe 531 x 1062
+		// Das panel hat die Größe 531 x 1062
 
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	
-		
+
 		dieSpielfigur = new Spielfigur();
-		
+
 		dieDoodleSteuerung = new DoodleSteuerung(dieSpielfigur, this);
 		dieDoodleSteuerung.erzeugePlattformen();
-		
+
 		dasDoodlePanel = dieDoodleSteuerung.getPanel();
 		contentPane.add(dasDoodlePanel, BorderLayout.CENTER);
-		
+
 		JButton btnStart = new JButton("Start");
-		//btnStart.setBounds(203, 400, 80, 40);
+		btnStart.setBounds(203, 400, 80, 40);
 		btnStart.setFocusable(false);
-		
-		contentPane.add(btnStart, BorderLayout.SOUTH);
-		
+
+		contentPane.add(btnStart, null);
+
 		setContentPane(contentPane);
 		btnStart.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+
 				tim.start();
-				//contentPane.remove(btnStart);
-			
-		
-	
-				
+				btnStart.setVisible(false);
+				// contentPane.remove(btnStart);
+
 			}
 		});
-		JButton btnPause = new JButton("Pause");
-		btnPause.setBounds(203, 450, 80, 40);
-		btnPause.setFocusable(false);
-		btnPause.setVisible(true);
-		contentPane.add(btnPause);
-		
-		setContentPane(contentPane);
-		btnPause.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				tim.stop();
-			
-			
-		
 	
-				
-			}
-		});
-		
-		
+
+
 		contentPane.add(dasDoodlePanel);
-		
+
 		dieDoodleSteuerung.erzeugePlattformen();
-		
+
 		tim = new Timer(10, this);
 		dieDoodleSteuerung.verarbeiteTimerEvent();
 		this.addKeyListener(this);
-		
+
 	}
-	
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		dieDoodleSteuerung.verarbeiteTimerEvent();
@@ -147,12 +126,24 @@ public class DoodleGUI extends JFrame implements ActionListener,  KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		switch(e.getKeyCode()) {
-		
-		case 39: dieSpielfigur.setBewege(1);
-		break;
-		case 37: dieSpielfigur.setBewege(2);
-		break;
+
+		switch (e.getKeyCode()) {
+
+		case 39:
+			dieSpielfigur.setBewege(1);
+			break;
+		case 37:
+			dieSpielfigur.setBewege(2);
+			break;
+		case 27:
+			if (tim.isRunning()) {
+				tim.stop();
+			} else {
+			
+				tim.start();
+			}
+
+			break;
 		}
 	}
 
@@ -160,7 +151,7 @@ public class DoodleGUI extends JFrame implements ActionListener,  KeyListener {
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 		dieSpielfigur.setBewege(0);
-		
+
 	}
 
 }
